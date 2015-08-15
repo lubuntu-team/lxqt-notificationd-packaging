d826353e221c1ed37e69058bcbc2808f4e393ca5
Andrew Lee (李健秋) <ajqlee@debian.org>
2015-08-16 03:06:50 +0800

    Merging upstream version 0.9.0+20150807.

--- config/basicsettings.cpp
+++ config/basicsettings.cpp
@@ -39,28 +39,26 @@ BasicSettings::BasicSettings(LxQt::Settings* settings, QWidget *parent) :
 
     restoreSettings();
 
-    connect(topLeftButton, SIGNAL(clicked()), this, SLOT(updateNotification()));
-    connect(topRightButton, SIGNAL(clicked()), this, SLOT(updateNotification()));
-    connect(bottomRightButton, SIGNAL(clicked()), this, SLOT(updateNotification()));
-    connect(bottomLeftButton, SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(topLeftRB,      SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(topCenterRB,    SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(topRightRB,     SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(centerLeftRB,   SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(centerCenterRB, SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(centerRightRB,  SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(bottomLeftRB,   SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(bottomCenterRB, SIGNAL(clicked()), this, SLOT(updateNotification()));
+    connect(bottomRightRB,  SIGNAL(clicked()), this, SLOT(updateNotification()));
 
     LxQt::Notification serverTest;
     QString serverName = serverTest.serverInfo().name;
     if (serverName != "lxqt-notificationd")
     {
         if (serverName.isEmpty())
-        {
-            warningLabel->setText(tr(
-                "<b>Warning:</b> No notifications daemon is running.\n"
-                "A fallback will be used."));
-        }
+            warningLabel->setText(tr("<b>Warning:</b> No notifications daemon is running.\n"
+            "A fallback will be used."));
         else
-        {
-            warningLabel->setText(tr(
-                "<b>Warning:</b> A third-party notifications daemon (%1) is running.\n"
-                "These settings won't have any effect on it!"
-                ).arg(serverName));
-        }
+            warningLabel->setText(tr("<b>Warning:</b> A third-party notifications daemon (%1) is running.\n"
+            "These settings won't have any effect on it!").arg(serverName));
     }
 }
 
@@ -70,33 +68,53 @@ BasicSettings::~BasicSettings()
 
 void BasicSettings::restoreSettings()
 {
-    QString placement = mSettings->value("placement", "bottom-right").toString().toLower();
-    if (placement == "bottom-right")
-        bottomRightButton->setChecked(true);
-    else if (placement == "bottom-left")
-        bottomLeftButton->setChecked(true);
-    else if (placement == "top-right")
-        topRightButton->setChecked(true);
-    else if (placement == "top-left")
-        topLeftButton->setChecked(true);
-    else
-        bottomRightButton->setChecked(true);
+    QString placement = mSettings->value(QStringLiteral("placement"),
+                                         QStringLiteral("bottom-right")).toString().toLower();
+
+    if (QStringLiteral("top-left") == placement)
+        topLeftRB->setChecked(true);
+    else if (QStringLiteral("top-center") == placement)
+        topCenterRB->setChecked(true);
+    else if (QStringLiteral("top-right") == placement)
+        topRightRB->setChecked(true);
+    else if (QStringLiteral("center-left") == placement)
+        centerLeftRB->setChecked(true);
+    else if (QStringLiteral("center-center") == placement)
+        centerCenterRB->setChecked(true);
+    else if (QStringLiteral("center-right") == placement)
+        centerRightRB->setChecked(true);
+    else if (QStringLiteral("bottom-left") == placement)
+        bottomLeftRB->setChecked(true);
+    else if (QStringLiteral("bottom-center") == placement)
+        bottomCenterRB->setChecked(true);
+    else if (QStringLiteral("bottom-right") == placement)
+        bottomRightRB->setChecked(true);
 }
 
 void BasicSettings::updateNotification()
 {
-    if (bottomRightButton->isChecked())
-        mSettings->setValue("placement", "bottom-right");
-    else if (bottomLeftButton->isChecked())
-        mSettings->setValue("placement", "bottom-left");
-    else if (topRightButton->isChecked())
-        mSettings->setValue("placement", "top-right");
-    else if (topLeftButton->isChecked())
-        mSettings->setValue("placement", "top-left");
+    QString align;
+    if (topLeftRB->isChecked())
+        align = QStringLiteral("top-left");
+    else if (topCenterRB->isChecked())
+        align = QStringLiteral("top-center");
+    else if (topRightRB->isChecked())
+        align = QStringLiteral("top-right");
+    else if (centerLeftRB->isChecked())
+        align = QStringLiteral("center-left");
+    else if (centerCenterRB->isChecked())
+        align = QStringLiteral("center-center");
+    else if (centerRightRB->isChecked())
+        align = QStringLiteral("center-right");
+    else if (bottomLeftRB->isChecked())
+        align = QStringLiteral("bottom-left");
+    else if (bottomCenterRB->isChecked())
+        align = QStringLiteral("bottom-center");
+    else // if (bottomRightRB->isChecked())
+        align = QStringLiteral("bottom-right");
 
-    LxQt::Notification::notify(//"lxqt-config-notificationd",
-                              tr("Notification demo"),
-                              tr("This is a test notification.\n"
-                                 "All notifications will now appear here on LXQt."),
-                              "lxqt-logo.png");
+    mSettings->setValue(QStringLiteral("placement"), align);
+    LxQt::Notification::notify(tr("Notification demo ") + align,
+                               tr("This is a test notification.\n All notifications will now appear here on LXQt."),
+                               QStringLiteral("lxqt-logo.png"));
 }
--- config/translations/lxqt-config-notificationd_de.ts
+++ config/translations/lxqt-config-notificationd_de.ts
@@ -1,48 +1,48 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
 <context>
     <name>AdvancedSettings</name>
     <message>
         <location filename="../advancedsettings.ui" line="17"/>
         <source>Sizes</source>
-        <translation type="unfinished"></translation>
+        <translation>Größen</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="25"/>
         <source>Width:</source>
-        <translation type="unfinished"></translation>
+        <translation>Breite:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="32"/>
         <location filename="../advancedsettings.ui" line="56"/>
         <source> px</source>
-        <translation type="unfinished"></translation>
+        <translation> px</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="49"/>
         <source>Spacing:</source>
-        <translation type="unfinished"></translation>
+        <translation>Abstand:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="68"/>
         <source>Duration</source>
-        <translation type="unfinished"></translation>
+        <translation>Dauer</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="74"/>
         <source>Some notifications set their own on-screen duration.</source>
-        <translation type="unfinished"></translation>
+        <translation>Einige Benachrichtigungen setzen ihre eigene Anzeigedauer.</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="86"/>
         <source>Default duration:</source>
-        <translation type="unfinished"></translation>
+        <translation>Standarddauer:</translation>
     </message>
     <message>
         <location filename="../advancedsettings.ui" line="93"/>
         <source> sec</source>
-        <translation type="unfinished"></translation>
+        <translation> s</translation>
     </message>
 </context>
 <context>
@@ -50,35 +50,38 @@
     <message>
         <location filename="../basicsettings.ui" line="23"/>
         <source>Basic Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Grundeinstellungen</translation>
     </message>
     <message>
         <location filename="../basicsettings.ui" line="30"/>
         <source>Position on screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Position auf dem Bildschirm</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="57"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; No notifications daemon is running.
 A fallback will be used.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Warnung:&lt;/b&gt; Es läuft kein Benachrichtigungsdaemon.
+Ein Ersatz wird verwendet.</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="60"/>
         <source>&lt;b&gt;Warning:&lt;/b&gt; A third-party notifications daemon (%1) is running.
 These settings won&apos;t have any effect on it!</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Warnung:&lt;/b&gt; Ein anderer Benachrichtigungsdaemon (%1) läuft.
+Diese Einstellungen wirken sich auf ihn nicht aus!</translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="117"/>
         <source>Notification demo </source>
-        <translation type="unfinished"></translation>
+        <translation>Testbenachrichtigung </translation>
     </message>
     <message>
         <location filename="../basicsettings.cpp" line="118"/>
         <source>This is a test notification.
  All notifications will now appear here on LXQt.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dies ist eine Testbenachrichtigung.
+Alle Benachrichtigungen erscheinen jetzt hier auf LXQt.</translation>
     </message>
 </context>
 <context>
@@ -86,17 +89,17 @@ These settings won&apos;t have any effect on it!</source>
     <message>
         <location filename="../mainwindow.cpp" line="40"/>
         <source>Desktop Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Arbeitsflächenbenachrichtigungen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="43"/>
         <source>Basic Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Grundeinstellungen</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="47"/>
         <source>Advanced Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Erweiterte Einstellungen</translation>
     </message>
 </context>
 </TS>
